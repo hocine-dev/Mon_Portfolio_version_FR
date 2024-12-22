def1d7f4801c9970dc22e6080b2c290e58e4f4e7
hocine dev <hocinedev4@gmail.com>
2024-12-22 16:37:41 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,36 @@ import heartIcon from "../../public/images/Footer/red-heart-icon.svg";
 import NextIcon from "../../public/images/Footer/next.svg";
 import tailwindIcon from "../../public/images/Footer/tailwindcss.svg";
 import Image from "next/image";
-import HomeStyle from "../styles/Home.module.css"
+import HomeStyle from "../styles/Home.module.css";
 
 const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg ">
       <Layout className="py-8 flex items-center justify-between lg:flex lg:flex-col xs:p-1">
         <span className="flex xs:text-sm xs:min-w-[100vw] xs:mt-5 xs:justify-center">
-          © 2023 Made With&nbsp;
-          <Image src={heartIcon} alt="heartIcon" className={`w-5 ${HomeStyle.rotate}`}></Image>&nbsp;
-          By&nbsp;
+          © 2025 Créé avec&nbsp;
+          <Image
+            src={heartIcon}
+            alt="heartIcon"
+            className={`w-5 ${HomeStyle.rotate}`}
+          ></Image>
+          &nbsp; Par&nbsp;
           <Link
             style={{ color: "#0a66c2" }}
             href="https://bit.ly/46G9YNw"
             target="blank"
           >
-            Hocine Dev
+            Hocine HAMAMA
           </Link>
         </span>
         <span className="flex items-center xs:text-sm xs:min-w-[100vw] xs:flex-col xs:pt-5">
-          Build with &nbsp;
-          <Image src={NextIcon} alt="nextjs icon" className="w-20 xs:pt-5"></Image>
-          &nbsp;&
+          Construit avec&nbsp;
+          <Image
+            src={NextIcon}
+            alt="nextjs icon"
+            className="w-20 xs:pt-5"
+          ></Image>
+          &nbsp;Et
           <Image
             src={tailwindIcon}
             alt="tailwindcss Icon"
@@ -34,8 +42,12 @@ const Footer = () => {
           ></Image>
           &nbsp;
         </span>
-        <Link  href="https://bit.ly/44m5euU" target="blank" className={`xs:text-sm xs:min-w-[100vw] text-center xs:mb-5 ${HomeStyle.linkHover}` }>
-        &#8594;&nbsp;Create your Portfolio Now&nbsp;&#8592;
+        <Link
+          href="https://bit.ly/44m5euU"
+          target="blank"
+          className={`xs:text-sm xs:min-w-[100vw] text-center xs:mb-5 ${HomeStyle.linkHover}`}
+        >
+          &#8594;&nbsp;Créez votre site web maintenant.&nbsp;&#8592;
         </Link>
       </Layout>
     </footer>
